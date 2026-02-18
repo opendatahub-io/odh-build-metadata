bc7381813e4d5f715f34ef4e0a00a046d6e46455
Deepak Chourasia <dchouras@redhat.com>
2026-02-18 23:19:24 +0530

    deleting bulk data

--- components/odh-operator/80d4e02c017698a3e055e0eb6f76c147c7d0606a65d1ffcab945d2e9c862ec7e/manifests/modelregistry/jobs-async-upload/examples/check-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "server: $(oc whoami --show-server)"
-echo "user: $(oc whoami)"
-echo "project: $(oc project --short)"
--- components/odh-operator/docs/README.md
+++
@@ -1,2 +0,0 @@
-# odh-build-metadata
-Used for storing ODH builds metadata
