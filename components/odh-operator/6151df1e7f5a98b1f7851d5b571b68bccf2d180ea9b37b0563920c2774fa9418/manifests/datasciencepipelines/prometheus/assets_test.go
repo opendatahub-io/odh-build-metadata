25a464860f8b4ac6439e68a904a0b1b89e8e8bf9
OpenShift-AI-DevOps <openshift-ai-devops@redhat.com>
2026-09-25 20:45:32 +0000

    build-meta for operator image 6151df1e7f5a98b1f7851d5b571b68bccf2d180ea9b37b0563920c2774fa9418

---
+++ components/odh-operator/6151df1e7f5a98b1f7851d5b571b68bccf2d180ea9b37b0563920c2774fa9418/manifests/datasciencepipelines/prometheus/assets_test.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2026.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package prometheus_test
+
+import (
+	"testing"
+
+	monitoringassets "github.com/opendatahub-io/data-science-pipelines-operator/config/prometheus"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/kustomize/api/krusty"
+	"sigs.k8s.io/kustomize/kyaml/filesys"
+)
+
+func TestRuleUsesModuleNamespace(t *testing.T) {
+	rule, err := monitoringassets.Rule("opendatahub")
+	require.NoError(t, err)
+	require.Equal(t, "monitoring.rhobs/v1", rule.GetAPIVersion())
+	require.Equal(t, "PrometheusRule", rule.GetKind())
+	require.Equal(t, monitoringassets.RuleName, rule.GetName())
+	require.Equal(t, "opendatahub", rule.GetNamespace())
+
+	groups, found, err := unstructured.NestedSlice(rule.Object, "spec", "groups")
+	require.NoError(t, err)
+	require.True(t, found)
+	for _, group := range groups {
+		for _, item := range group.(map[string]interface{})["rules"].([]interface{}) {
+			labels := item.(map[string]interface{})["labels"].(map[string]interface{})
+			require.Equal(t, "opendatahub", labels["namespace"])
+		}
+	}
+}
+
+func TestServiceMonitorAssets(t *testing.T) {
+	core, err := monitoringassets.CoreServiceMonitor("opendatahub")
+	require.NoError(t, err)
+	require.Equal(t, "monitoring.coreos.com/v1", core.GetAPIVersion())
+	require.Equal(t, monitoringassets.CoreServiceMonitorName, core.GetName())
+	require.Equal(t, "opendatahub", core.GetNamespace())
+	endpoints, found, err := unstructured.NestedSlice(core.Object, "spec", "endpoints")
+	require.NoError(t, err)
+	require.True(t, found)
+	serverName := endpoints[0].(map[string]interface{})["tlsConfig"].(map[string]interface{})["serverName"].(string)
+	require.Equal(t, "data-science-pipelines-operator-service.opendatahub.svc", serverName)
+
+	rhoai, err := monitoringassets.RHOAIServiceMonitor("custom-applications", "custom-monitoring")
+	require.NoError(t, err)
+	require.Equal(t, "monitoring.rhobs/v1", rhoai.GetAPIVersion())
+	require.Equal(t, monitoringassets.RHOAIServiceMonitorName, rhoai.GetName())
+	require.Equal(t, "custom-monitoring", rhoai.GetNamespace())
+	matchNames, found, err := unstructured.NestedStringSlice(rhoai.Object, "spec", "namespaceSelector", "matchNames")
+	require.NoError(t, err)
+	require.True(t, found)
+	require.Equal(t, []string{"custom-applications"}, matchNames)
+	endpoints, found, err = unstructured.NestedSlice(rhoai.Object, "spec", "endpoints")
+	require.NoError(t, err)
+	require.True(t, found)
+	serverName = endpoints[0].(map[string]interface{})["tlsConfig"].(map[string]interface{})["serverName"].(string)
+	require.Equal(t, "data-science-pipelines-operator-service.custom-applications.svc", serverName)
+
+	binding, err := monitoringassets.RHOAIMetricsReaderRoleBinding("custom-monitoring")
+	require.NoError(t, err)
+	require.Equal(t, monitoringassets.RHOAIMetricsReaderRoleBindingName, binding.GetName())
+	subjects, found, err := unstructured.NestedSlice(binding.Object, "subjects")
+	require.NoError(t, err)
+	require.True(t, found)
+	require.Equal(t, "custom-monitoring", subjects[0].(map[string]interface{})["namespace"])
+}
+
+func TestPlatformModuleOverlaysExcludeMonitoringResources(t *testing.T) {
+	for _, overlay := range []string{"odh", "rhoai"} {
+		t.Run(overlay, func(t *testing.T) {
+			kustomizer := krusty.MakeKustomizer(krusty.MakeDefaultOptions())
+			resources, err := kustomizer.Run(filesys.MakeFsOnDisk(), "../overlays/"+overlay+"/dspo")
+			require.NoError(t, err)
+			rendered, err := resources.AsYaml()
+			require.NoError(t, err)
+			require.NotContains(t, string(rendered), "kind: ServiceMonitor")
+			require.NotContains(t, string(rendered), "kind: PrometheusRule")
+		})
+	}
+}
